c9f4f27e4688803a405484c8e8dd50db8394a23a
James Letendre <Imajie@gmail.com>
2012-07-28 10:40:18 -0400

    added missing files

---
+++ src/tileset.h
@@ -0,0 +1,28 @@
+/*
+ * File:	tileset.h
+ * Author:	James Letendre
+ *
+ * Constants for tileset indicies
+ */
+#ifndef _TILESET_H_
+#define _TILESET_H_
+
+#define TILESET_SIZE	(64)
+
+// single tile types (index in tileset)
+#define GRASS_ID	(0)
+#define LAVA_ID		(999)
+#define EMPTY_ID	(0)
+
+// multi tile types -(start_index)
+#define PATH_ID		(-1)
+#define WALL_ID		(-999)
+
+// Mask for multi visual tiles
+#define MULTI_N	(8)
+#define MULTI_S	(4)
+#define MULTI_E	(2)
+#define MULTI_W	(1)
+
+
+#endif
